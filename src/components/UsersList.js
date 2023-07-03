3de553d6018776afffa54a4436520db12946ba3c
Caylin James <cjames@leapagency.com>
2023-07-03 15:40:46 -0400

    a taste of moving all loading and error state into components manually

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,23 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from "../store";
+import { fetchUsers, addUser } from "../store";
+import Button from "./Button";
 import Skeleton from "./Skeleton";
 
 const UsersList = () => {
+    const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+    const [loadingUsersError, setLoadingUsersError] = useState(null);
+    const [isCreatingUser, setIsCreatingUser] = useState(false);
+    const [creatingUserError, setCreatingUserError] = useState(null);
     const dispatch = useDispatch();
-    const { isLoading, data, error } = useSelector((state) => {
+    const { data } = useSelector((state) => {
         return state.users;
     });
 
     useEffect(() => {
-        dispatch(fetchUsers());
+        setIsLoadingUsers(true);
+        dispatch(fetchUsers())
+            .unwrap()
+            .catch((err) => setLoadingUsersError(err))
+            .finally(() => setIsLoadingUsers(false));
     }, [dispatch]);
 
-    if (isLoading) {
+    const handleUserAdd = () => {
+        setIsCreatingUser(true);
+        dispatch(addUser())
+            .unwrap()
+            .catch((err) => setCreatingUserError(err))
+            .finally(() => setIsCreatingUser(false));
+    };
+
+    if (isLoadingUsers) {
         return <Skeleton times={6} className="h-10 w-full" />;
     }
 
-    if (error) {
+    if (loadingUsersError) {
         return <div>Error fecting data...</div>;
     }
 
@@ -31,7 +48,20 @@ const UsersList = () => {
         );
     });
 
-    return <div>{renderedUsers}</div>;
+    return (
+        <div>
+            <div className="flex flex-row justify-between m-3">
+                <h1 className="m-2 text-xl">Users</h1>
+                {isCreatingUser ? (
+                    "Creating User..."
+                ) : (
+                    <Button onClick={handleUserAdd}>+ Add User</Button>
+                )}
+                {creatingUserError && "Error creating user..."}
+            </div>
+            {renderedUsers}
+        </div>
+    );
 };
 
 export default UsersList;
