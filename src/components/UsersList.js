cc6cfe21b49c570c0888e3e476bdbc80cc73b888
Caylin James <cjames@leapagency.com>
2023-07-03 14:23:20 -0400

    add in a skeleton loader animation

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
+import Skeleton from "./Skeleton";
 
 const UsersList = () => {
     const dispatch = useDispatch();
@@ -13,7 +14,7 @@ const UsersList = () => {
     }, [dispatch]);
 
     if (isLoading) {
-        return <div>Loading...</div>;
+        return <Skeleton times={6} className="h-10 w-full" />;
     }
 
     if (error) {
