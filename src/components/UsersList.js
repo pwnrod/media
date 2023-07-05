fb78b56fd5c1c792e8d17c48a1257fe3cf08c43b
Caylin James <cjames@leapagency.com>
2023-07-05 09:23:27 -0400

    finish up working with async thunks manually

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -4,54 +4,49 @@ import { fetchUsers, addUser } from "../store";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
 import { useThunk } from "../hooks/use-thunk";
+import UsersListItem from "./UsersListItem";
 
 const UsersList = () => {
-    const [doFetchUsers, isLoadingUsers, loadingUsersError] =
-        useThunk(fetchUsers);
-    const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] =
+    useThunk(fetchUsers);
+  const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
 
-    const { data } = useSelector((state) => {
-        return state.users;
+  const { data } = useSelector((state) => {
+    return state.users;
+  });
+
+  useEffect(() => {
+    doFetchUsers();
+  }, [doFetchUsers]);
+
+  const handleUserAdd = () => {
+    doCreateUser();
+  };
+
+  let content;
+
+  if (isLoadingUsers) {
+    content = <Skeleton times={6} className="h-10 w-full" />;
+  } else if (loadingUsersError) {
+    content = <div>Error fecting data...</div>;
+  } else {
+    content = data.map((user) => {
+      return <UsersListItem key={user.id} user={user} />;
     });
+  }
 
-    useEffect(() => {
-        doFetchUsers();
-    }, [doFetchUsers]);
-
-    const handleUserAdd = () => {
-        doCreateUser();
-    };
-
-    let content;
-
-    if (isLoadingUsers) {
-        content = <Skeleton times={6} className="h-10 w-full" />;
-    } else if (loadingUsersError) {
-        content = <div>Error fecting data...</div>;
-    } else {
-        content = data.map((user) => {
-            return (
-                <div key={user.id} className="mb-2 border rounded">
-                    <div className="flex p-2 justify-between items-center cursor-pointer">
-                        {user.name}
-                    </div>
-                </div>
-            );
-        });
-    }
-
-    return (
-        <div>
-            <div className="flex flex-row justify-between items-center m-3">
-                <h1 className="m-2 text-xl">Users</h1>
-                <Button loading={isCreatingUser} onClick={handleUserAdd}>
-                    + Add User
-                </Button>
-                {creatingUserError && "Error creating user..."}
-            </div>
-            {content}
-        </div>
-    );
+  return (
+    <div>
+      <div className="flex flex-row justify-between items-center m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        <Button loading={isCreatingUser} onClick={handleUserAdd}>
+          + Add User
+        </Button>
+        {creatingUserError && "Error creating user..."}
+      </div>
+      {content}
+    </div>
+  );
 };
 
 export default UsersList;
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import { usersReducers } from "./slices/usersSlice";
 
 export const store = configureStore({
-    reducer: {
-        users: usersReducers,
-    },
+  reducer: {
+    users: usersReducers,
+  },
 });
 
 export * from "./thunks/fetchUsers";
 export * from "./thunks/addUser";
+export * from "./thunks/removeUser";
